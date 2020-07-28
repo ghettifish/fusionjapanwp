bdd965c1ef93a87deedbf65ca3f11545023c7b31
Nicolas Bovee <nicolasbovee@Nics-MacBook-Pro.local>
2020-07-28 15:40:44 -0600

    add sorting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,96 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import { graphql } from 'gatsby'
-  import { css, cx } from 'emotion'
+import { css, cx } from 'emotion'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Img from "gatsby-image"
 import Chili from "../components/chili"
+import FlipMove from "react-flip-move"
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Fusion Japan Woodland Park Menu" description="Fan-made menu for Fusion Japan restaurant in Woodland Park Colorado"/>
-    <div>
-      {data.allContentfulMenuCategory.nodes.map(x => (
-        <div key={x.id}>  
-          <Category data={data} name={x.categoryName} />
-        </div>
-      ))
-      }
-    </div>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const [categories, setCategories] = useState(data.allContentfulMenuCategory.nodes);
+
+  const allItems = () => {
+    setCategories(data.allContentfulMenuCategory.nodes);
+  }
+  const flip = (category) => {
+
+    setCategories([{ categoryName: category.categoryName, id: category.id }])
+
+  }
+  return (
+
+    <Layout>
+      <SEO title="Fusion Japan Woodland Park Menu" description="Fan-made menu for Fusion Japan restaurant in Woodland Park Colorado" />
+      <h3 className={css`
+       text-transform: uppercase;
+       letter-spacing: 3px;
+       font-weight: 100;
+       margin-top: 50px;`}>Sort menu by:</h3>
+      <ul className={css`
+      display: inline !important;
+      list-style: none;
+      margin-left: 0;
+      font-size: 14px;
+      `}>
+        <li className={css`display: inline; margin-right: 20px; font-family: sans-serif;`}><a href="#" className={css`text-decoration: none; color: #8c8686;`}onClick={allItems}>All Items</a></li>
+        {data.allContentfulMenuCategory.nodes.map(x => (
+          <li className={css`display: inline; margin-right: 20px; font-family: sans-serif;`}><a href="#" className={css`text-decoration: none; color: #8c8686;`}onClick={() => flip(x)}>{x.categoryName}</a></li>
+        ))
+        }
+      </ul>
+      <div>
+        <FlipMove>
+          {categories.map(x => (
+            <div key={x.id}>
+              <Category data={data} name={x.categoryName} />
+            </div>
+          ))
+          }
+        </FlipMove>
+      </div>
+    </Layout>
+  )
+}
+
 
 const Category = ({ name, data }) => {
-  if (data.allContentfulMainMenuItems.nodes.filter(y => y.menuCategory && y.menuCategory.categoryName === name).length > 0) {
+
+  const [mainMenuItems, setMainMenuItems] = useState(data.allContentfulMainMenuItems.nodes);
+  const [sushi, setSushi] = useState(data.allContentfulSushi.nodes);
+  const reverseMenuItems = () => {
+    alert("clicked");
+    setMainMenuItems(mainMenuItems.reverse())
+  }
+  const reverseSushi = () => {
+    alert("clicked");
+    setSushi(sushi.reverse())
+  }
+  if (mainMenuItems.filter(y => y.menuCategory && y.menuCategory.categoryName === name).length > 0) {
     return (
       <div>
-        <MenuTitle title={name}/>      
-        {data.allContentfulMainMenuItems.nodes.filter(y => y.menuCategory && y.menuCategory.categoryName === name).map(x => (
-          <MenuItem id={x.id} name={x.name} description={x.description.description} spicy={x.spicy} />
-        ))
-        }
+        <MenuTitle title={name} />
+        <FlipMove>
+          {mainMenuItems.filter(y => y.menuCategory && y.menuCategory.categoryName === name).map(x => (
+            <MenuItem id={x.id} name={x.name} description={x.description.description} spicy={x.spicy} />
+          ))
+          }
+        </FlipMove>
       </div>
     )
-  } else if(data.allContentfulSushi.nodes.filter(y => y.menuCategory === name).length > 0){
+  } else if (sushi.filter(y => y.menuCategory === name).length > 0) {
     return (
       <div>
-        <MenuTitle title={name}/>      
-        {data.allContentfulSushi.nodes.filter(y => y.menuCategory === name).map(x => (
-          <MenuItem id={x.id} name={x.name} description={x.description.description} />
-        ))
-        }
+        <MenuTitle title={name} />
+        <FlipMove>
+
+          {sushi.filter(y => y.menuCategory === name).map(x => (
+            <MenuItem id={x.id} name={x.name} description={x.description.description} />
+          ))
+          }
+        </FlipMove>
       </div>
     )
   } else {
@@ -48,24 +98,24 @@ const Category = ({ name, data }) => {
   }
 }
 
-const MenuItem = ({id, name, description, spicy}) => (
+const MenuItem = ({ id, name, description, spicy }) => (
   <div key={id} className={css`
   padding: 10px;
 `}>
-  <h4 className={css`
+    <h4 className={css`
   margin: 0;
   text-transform: uppercase;
   color: #c39b7e;
   display: inline;
-`}>{name}{spicy && <Chili/>}</h4>
+`}>{name}{spicy && <Chili />}</h4>
 
-  <p className={css`
+    <p className={css`
   margin: 0;
 `}>{description}</p>
-</div>
+  </div>
 )
 
-const MenuTitle = ({title}) => (
+const MenuTitle = ({ title }) => (
   <h3 className={css`
   text-transform: uppercase;
   letter-spacing: 3px;
